696a7d714d8b53e4fe2e33b7b7ab1f39c91c73a0
agung <agunglaksmana908@gmail.com>
2022-03-24 21:58:16 +0700

    from read more to baca lagi

--- components/interface/card/index.jsx
+++ components/interface/card/index.jsx
@@ -48,7 +48,7 @@ const CardBerita = ({ data, title, subtitle }) => {
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                                                 </svg>
-                                                Read More
+                                                Baca Lagi
                                             </button>
                                             <div className="text-sm dark:text-white hidden md:block lg:block">
                                                 {pecah.author}
