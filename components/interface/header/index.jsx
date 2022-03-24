204baeda3079d7d6fc423cc9c8c8d09ed58b38db
agung <agunglaksmana908@gmail.com>
2022-03-24 22:07:20 +0700

    change name

--- components/interface/header/index.jsx
+++ components/interface/header/index.jsx
@@ -12,7 +12,7 @@ export default function Header() {
             <section className="container px-4 py-7 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center ">
                 <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
                     <h1 className="text-3xl leading-snug text-blue-500 dark:text-gray-200 md:text-4xl">
-                        <span className="font-semibold">{dataUser == null ? "Sagara Teknologi TestCase" : `Selamat Datang ${dataUser.firstname} ${dataUser.lastname}`}</span>
+                        <span className="font-semibold">{dataUser == null ? "Sagara Teknologi TestCase" : `Selamat Datang ${dataUser.firstname} ${dataUser.lastname}`} oke</span>
                     </h1>
                 </div>
                 <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
