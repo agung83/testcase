add02d50c4eab6c5564cde8d28a7dac0f47c7825
agung <agunglaksmana908@gmail.com>
2022-03-24 22:25:50 +0700

    paging dan tes

--- components/interface/pagination/index.jsx
+++ components/interface/pagination/index.jsx
@@ -19,7 +19,7 @@ const Pagination = ({ totalPages, paginate, Page }) => {
                     className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                 >
                     <span className="sr-only">Previous</span>
-                    Previous
+                    Previouss
                 </a>
                 {
                     paging.slice(0, 4 + Page).map(number => {
